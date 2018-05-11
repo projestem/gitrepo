dade0d37639c9077cef7d9b0519e80b3cf6c9efa
projestem <franko.rogowski@gmail.com>
2018-05-11 13:31:31 +0200

    baza

--- baza/szkola.sql
+++ baza/szkola.sql
@@ -21,3 +21,15 @@ CREATE TABLE tbKlasa
 		rokNaboru INTEGER,
 		rokMatury INTEGER
 );
+
+INSERT INTO tbKlasa(id, klasa, rokNaboru, rokMatury) VALUES (NULL, '1A', 2017, 2020);
+INSERT INTO tbKlasa VALUES (NULL, '2A', 2016, 2019);
+INSERT INTO tbKlasa VALUES (NULL, '1C', 2017, 2020);
+-- INSERT INTO tbUczniowie (id, klasa, nazwisko, plec, id_klasa)
+
+INSERT INTO tbUczniowie(id, imie, nazwisko, plec, id_klasa, egzHum, egzMat, egzJez)
+VALUES(NULL, 'Adam', 'Słodowy', 0, 3, 70.5, 80, 90);
+INSERT INTO tbUczniowie(id, imie, nazwisko, plec, id_klasa, egzHum, egzMat, egzJez)
+VALUES(NULL, 'Franciszek', 'Rogowski', 0, 1, 100, 100, 100);
+
+UPDATE tbUczniowie SET egzJez = 100 WHERE id = 1;
