a669f99f8dd56105d20c227b7fe8293b4b8f090b
MARIAMJESTEM <mariamazur.2000@wp.pl>
2018-09-10 08:42:47 +0200

    plan

---
+++ cpp/szablon.cpp
@@ -0,0 +1,20 @@
+/*
+ * szablon.cpp
+ */
+
+
+#include <iostream>
+
+using namespace
+
+int main(int argc, char **argv)
+{
+    int a; // deklaracja zmiennej
+	a = 0;   // inicacja zmiennej
+    cout << "Podaj liczbe: ";
+    cin >> a;
+    cout << a;
+    return 0;
+    
+}
+
