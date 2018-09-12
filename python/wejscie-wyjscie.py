4332d2c6ad625b8497ad28ecd9fe1767b4d6688a
MARIAMJESTEM <mariamazur.2000@wp.pl>
2018-09-12 10:29:03 +0200

    cpp

--- python/wejscie-wyjscie.py
+++ python/wejscie-wyjscie.py
@@ -6,12 +6,21 @@
 
 def main(args):
     # a = 0 # inicjacja zmiennej
-    a = input('Podaj liczbę')
+    a = int(input('Podaj liczbę'))
     print(a)
-    b = input("Podaj 1. liczbę: ")
+    b = int(input("Podaj 1. liczbę: "))
     print(b)
     
+    print("Suma: ", a + b)
+    print("Różnica: ", a - b)
+    print("Iloczyn: ", a * b)
+    print("Iloraz: ", a / b)
+
+    
+    
     return 0
+    
+# duck typing
 
 if __name__ == '__main__':
     import sys
