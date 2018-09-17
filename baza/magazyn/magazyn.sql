ab1cde3092c42cc1d7ea0c47b882372b8ae074f7
MARIAMJESTEM <mariamazur.2000@wp.pl>
2018-09-17 08:44:00 +0200

    baza

---
+++ baza/magazyn/magazyn.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    adress TEXT
+);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    customer_id INTEGER,
+    subscription_id INTEGER,
+    purchase_date DATE
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    FOREIGN KEY (subscription_id) REFERENCES subscriptions(id)
+);
+
+DROP TABLE IF EXISTS subscriptions;
+CREATE TABLE subscriptions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    description TEXT,
+    price_per_month DECIMAL,
+    subscription_length TEXT
+);
