593cb4f87b60fa4ebee27ef89a3c76ace562af07
MARIAMJESTEM <mariamazur.2000@wp.pl>
2018-09-20 08:43:47 +0200

    jupyter

---
+++ python/petla-for.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  petla-for.py
+
+
+def main(args):
+    start = int(input("Start: ""))
+    stop = int(input("Stop: "))
+
+    while warunek:
+        pass
+    
+    if start >= stop:
+        print("Błędny zakres!")
+    
+    for liczba in range(start, stop + 1):
+        if liczba % 2 == 0;
+            print(liczba)
+
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
