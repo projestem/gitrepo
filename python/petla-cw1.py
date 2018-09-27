207312270c633c2ee25e6f172891ab04725d5f1f
MARIAMJESTEM <mariamazur.2000@wp.pl>
2018-09-27 08:43:29 +0200

    petle

---
+++ python/petla-cw1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  petla-cw1.py
+
+
+def main(args):
+    suma = 0
+    
+    while suma <= 75:
+        liczba = int(input("Podaj liczbę: "))
+        suma = 
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
