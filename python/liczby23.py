720780520801332aa1a9e42a0efeeded0b45ddca
projestem <franko.rogowski@gmail.com>
2018-10-10 23:07:12 +0200

    male poprawki i praca domowa

--- python/liczby23.py
+++ python/liczby23.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  liczby2-3.py
-# 
+#  liczby23.py
 
-
-def liczby2(a=10, b=99):
+def liczby2():
+    """
+    Funkcja drukuje wszystkie liczby 2-cyfrowe, w których cyfry nie powtarzają się. Funkcja zwraca ich liczbę.
+    Wykluczone liczby: 11, 22, 33 itd.
+    """
     
-    for liczba in range(10, 99):
-        if (liczba % 11) != 0:
-            print(liczba)
-        print()
-			          
-
+    ile = 0
+    
+    for i in range(1, 10):
+        for j in range(0, 10):
+            if i == j:
+                print("", end = '')
+            else:
+                print("{}{} ".format(i,j), end='')
+                ile = ile + 1
+    
+    return ile
+    
+def liczby3():
+    """
+    Funkcja drukuje wszystkie liczby 3-cyfrowe, w których cyfry nie powtarzają się. Funkcja zwraca ich liczbę.
+    Wykluczone liczby: 111, 222, 333, 110, 211 itd.
+    """
+    
+    ile = 0
+    
+    for i in range(1, 10):
+        for j in range(0, 10):
+            for k in range(0, 10):
+                if i == j or i == k or k == j:
+                    print("", end = '')
+                else:
+                    print("{}{}{} ".format(i,j,k), end='')
+                    ile = ile + 1
+    
+    return ile
 
 def main(args):
-    
-    print(liczby2())
-    
+    print("Liczby 2-cyfrowych: ", liczby3())
     return 0
 
 if __name__ == '__main__':
     import sys
-sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv))
