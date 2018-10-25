1fecc94e6413904b2829ae5055011924d6524ad7
MARIAMJESTEM <mariamazur.2000@wp.pl>
2018-10-25 08:45:15 +0200

    gdzie sie podzialy tamte prywatki

---
+++ cpp/minmax/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int minmax1()
+{
+    int min, max;
+    cout << "Podaj liczbę: "; << endl;
+    cin >> liczba;
+    min = max = liczba;
+    while(dalej == 't' ) {
+        cout << "Podaj liczbę: ";
+        cin >> liczba;
+        if (liczba < min)
+
+        if (liczba > max)
+            max = liczba;
+        cout << "Następna (t/n)? ";
+        cin >> dalej;
+    }
+    cout << "Najmniejsza: " << min << endl;
+    cout << "Największa: " << max << endl;
+}
+
+    void wypelnij(int, tab[], int roz){
+        cout << "Podaj " << roz << liczb:"  << endl;
+        for(int i = 0; i < roz)
+    }
+
+
+
+int main()
+{
+    int tab[];
+    minmax1();
+
+}
