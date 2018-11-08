93dbf76aadbe39319f8dc7fa0e66b75b1de3b12b
MARIAMJESTEM <mariamazur.2000@wp.pl>
2018-11-08 08:45:13 +0100

    Patologiczne liczenie znakow

--- cpp/liczznaki.cpp
+++ cpp/liczznaki.cpp
@@ -1,41 +1,78 @@
 /*
- * liczznaki.cpp
- * 
- * Copyright 2018 kl2ag2 <kl2ag2@ubu02>
+ * znaki.cpp
  */
  
  
 #include <iostream>
+#include <string.h>
  
 using namespace std;
  
-void licz_znaki(char tb[], int roz){
-    //for(int i = 0; i < roz; i++){
-        //cout << tb[i];
-    //}
-    int i = 0;
-    int biale, literyD, literyM, reszta;
-    biale = literyD = literyM = reszta = 0;
-    int znak_kod = 0;
-    while(tb[i] != '\0'){
-        znak_kod = (int)tb[i];
-        if(znak_kod > 64 && znak_kod < 91)
-            literyD++;
-        else if(znak_kod > 96 && znak_kod < 123)
-            literyM++;
+void zamien_znaki(char tb[], int roz) {
+    // Zamień małe litery na duże
+    int i=0;
+    while(tb[i]!='\0'){
+        tb[i]=(char)((int)tb[i]-32);
         i++;
-        }
-    cout << "Białych: " << biale << endl;
-    cout << "Duże: " << literyD << endl;
-    cout << "Małe: " << literyM << endl;
-    cout << "Reszta: " << reszta << endl;
+    }
+    i=0;
+    while(tb[i]!='\0'){
+        cout<<tb[i];
+        i++;
+    }
 }
  
+void licz_znaki(char tb[], int roz){
+    int i = 0;
+    int cyfry, literyD, literyM, reszta;
+    cyfry = literyD = literyM = reszta = 0;
+ 
+    int znak_kod = 0; // kod ASCII badanego znaku
+    
+    while (tb[i] != '\0'){
+        znak_kod = (int)tb[i];
+        if(znak_kod > 64 && znak_kod < 91) literyD++;
+        else if (znak_kod > 96 && znak_kod < 123) literyM++;
+        else if (znak_kod > 47 && znak_kod < 58) cyfry++;
+        else reszta++;
+        i++; // Inkrementacja indeksu
+    }
+    cout << "Duże: " << literyD << endl
+    << "Małe: " << literyM << endl
+    << "Cyfry: " << cyfry << endl
+    << "Reszta: " << reszta << endl;
+}
+ 
+int zlicz(char tb[]){
+    int i = 0;
+    while (tb[i] != '\0') i++;
+    return i;
+}
+ 
+void ascii(char tb[], int roz){
+    int kod = 0;
+    for(int i = 0; i < roz; i++){
+        kod = (int)tb[i];
+        if (kod > 96 && kod <123)
+            cout << (char)(kod-32); << " ";
+        else
+            cout << tb [i] << " ";
+    }
+}
+
+void odwroc(){
+
 int main(int argc, char **argv)
 {
-    const int rozmiar = 21;
-    char znaki[rozmiar]; // max 20 znaków ostatni to \0
-    cin.getline(znaki, 21);
-    licz_znaki(znaki, rozmiar);
+    const int rozmiar = 20;
+    char znaki[rozmiar];
+    cin.getline(znaki, rozmiar);
+    //licz_znaki(znaki, rozmiar+1);
+    //zamien_znaki(znaki, rozmiar);
+    //ilosc = cin.gcount();
+    //ilosc = zlicz(znaki);
+    int ilosc = 0;
+    ilosc = strlen(znaki);
+    ascii(znaki, ilosc);
         return 0;
 }
