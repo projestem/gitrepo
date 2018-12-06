2e43fd964b4ba9fdfbce6d87f1a9c23b7d39c25a
MARIAMJESTEM <mariamazur.2000@wp.pl>
2018-12-06 08:42:13 +0100

    jarzebina czerwonagit add .

---
+++ cpp/silnia.cxx
@@ -0,0 +1,36 @@
+/*
+ * silnia.cxx
+ * 
+ * Copyright 2018  <>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int a;
+    cout << "Podaj liczbe";
+    cin >> a;
+    cout << "Silnia::" << silnia_it(a) << endl;
+    cout << "silnia:" << silnia_re(a) << endl;
+	return 0;
+}
+
